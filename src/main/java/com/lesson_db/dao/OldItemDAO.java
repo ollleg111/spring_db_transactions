45aac7b57005d77ae085305c0f49a1dfca9488db
oleg.maliy14@gmail.com <q2301299266>
2019-12-24 21:07:47 +0200

    -0- 24.12.2019

--- src/main/java/com/lesson_db/dao/OldItemDAO.java
+++ src/main/java/com/lesson_db/dao/OldItemDAO.java
@@ -5,54 +5,78 @@ import com.lesson_db.exceptions.BadRequestException;
 import org.hibernate.HibernateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.List;
 
+// реализация ItemDAO без аннотации @Transaction
 @Repository
-@Transactional
-public class ItemDAO {
+public class OldItemDAO {
+
     private Item item;
 
     @PersistenceContext
     EntityManager entityManager;
 
     @Autowired
-    public ItemDAO(Item item) {
+    public OldItemDAO(Item item) {
         this.item = item;
     }
 
     private static final String NAME_LIKE = "SELECT * FROM ITEMS WHERE ITEMS.NAME LIKE ?";
     private static final String SELECT_FROM = "SELECT * FROM ITEMS";
 
+    private EntityTransaction transaction = entityManager.getTransaction();
+
     public Item findById(long id) throws HibernateException {
         try {
+            transaction.begin();
             item = entityManager.find(Item.class, id);
+            transaction.commit();
         } catch (HibernateException e) {
             System.err.println("findById(long id) is failed");
             System.err.println(e.getMessage());
+
+            if (transaction != null)
+                transaction.rollback();
+            throw new HibernateException("The method findById(long id) was failed in class "
+                    + com.lesson_db.dao.ItemDAO.class.getName());
         }
         return item;
     }
 
     public void save(Item item) throws HibernateException {
         try {
+            transaction.begin();
             entityManager.persist(item);
+            transaction.commit();
         } catch (HibernateException e) {
             System.err.println("save(Item item) is failed");
             System.err.println(e.getMessage());
+
+            if (transaction != null)
+                transaction.rollback();
+            throw new HibernateException("The method save(Item item) was failed in class "
+                    + com.lesson_db.dao.ItemDAO.class.getName());
         }
     }
 
     public Item update(Item item) throws HibernateException {
         try {
+            transaction.begin();
             item = entityManager.merge(item);
+            transaction.commit();
         } catch (HibernateException e) {
             System.err.println("update(Item item) is failed");
             System.err.println(e.getMessage());
+
+            if (transaction != null)
+                transaction.rollback();
+            throw new HibernateException("The method update(Item item) was failed in class "
+                    + com.lesson_db.dao.ItemDAO.class.getName());
         }
         return item;
     }
@@ -71,16 +95,23 @@ public class ItemDAO {
         } catch (HibernateException e) {
             throw new HibernateException("Operation with item with name: " + name
                     + " was filed in method deleteByName(String name) from class "
-                    + ItemDAO.class.getName());
+                    + com.lesson_db.dao.ItemDAO.class.getName());
         }
     }
 
     public void deleteItem(Item item) throws HibernateException {
         try {
+            transaction.begin();
             entityManager.remove(item);
+            transaction.commit();
         } catch (HibernateException e) {
             System.err.println("deleteItem(Item item) is failed");
             System.err.println(e.getMessage());
+
+            if (transaction != null)
+                transaction.rollback();
+            throw new HibernateException("The method deleteItem(Item item) was failed in class "
+                    + com.lesson_db.dao.ItemDAO.class.getName());
         }
     }
 
@@ -92,7 +123,7 @@ public class ItemDAO {
             return query.getResultList();
         } catch (HibernateException e) {
             throw new HibernateException("Operation filed in method findAll() from class "
-                    + ItemDAO.class.getName());
+                    + com.lesson_db.dao.ItemDAO.class.getName());
         }
     }
 }
